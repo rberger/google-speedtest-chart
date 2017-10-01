23b66819e5358be1c6204d3ff56ca325f3119486
Robert J. Berger <rberger@ibd.com>
2017-10-01 00:25:29 -0700

    Added mechanism to allow for command line args. Added arg to have no regular output (for cron)

--- speedtest-charts.py
+++ speedtest-charts.py
@@ -7,6 +7,17 @@ import datetime
 import pygsheets
 import speedtest
 
+try:
+    from argparse import ArgumentParser as ArgParser
+    from argparse import SUPPRESS as ARG_SUPPRESS
+    PARSER_TYPE_INT = int
+    PARSER_TYPE_STR = str
+except ImportError:
+    from optparse import OptionParser as ArgParser
+    from optparse import SUPPRESS_HELP as ARG_SUPPRESS
+    PARSER_TYPE_INT = 'int'
+    PARSER_TYPE_STR = 'string'
+
 # Set constants
 DATE = datetime.datetime.utcnow().strftime("%m/%d/%y %H:%M:%S")
 
@@ -27,23 +38,73 @@ def submit_into_spreadsheet(download, upload, ping):
     sheet.append_table(values=data)
 
 def main():
+    args = parse_args()
+    if args.quiet:
+        quiet = True
+    else:
+        quiet = False
+
     # Check for proper credentials
-    print("Checking OAuth validity...")
+    printer("Checking OAuth validity...", quiet)
     credentials = get_credentials()
 
     # Run speedtest and store output
-    print("Starting speed test...")
+    printer("Starting speed test to RidgeWireless...", quiet)
+    servers = [12818] # RidgeWireless id hardwired for now
+
     spdtest = speedtest.Speedtest()
+    spdtest.get_servers(servers)
     spdtest.get_best_server()
     download = spdtest.download()
     upload = spdtest.upload()
     ping = spdtest.results.ping
-    print("Starting speed finished!")
+    printer("Starting speed finished!", quiet)
 
     # Write to spreadsheet
-    print("Writing to spreadsheet...")
+    printer("Writing to spreadsheet...", quiet)
     submit_into_spreadsheet(download, upload, ping)
-    print("Successfuly written to spreadsheet!")
+    printer("Successfuly written to spreadsheet!", quiet)
+
+def printer(string, quiet=False, debug=False, **kwargs):
+    """Helper function to print a string only when not quiet"""
+
+    if debug and not DEBUG:
+        return
+
+    if debug:
+        out = '\033[1;30mDEBUG: %s\033[0m' % string
+    else:
+        out = string
+
+    if not quiet:
+        print(out)
+
+
+def parse_args():
+    """Function to handle building and parsing of command line arguments"""
+    description = (
+        'Simple Python script to push speedtest results (using speedtest-cli) '
+        'to a Google Docs spreadsheet\n'
+        '-------------------------------------------------------------------\n')
+
+
+    parser = ArgParser(description=description)
+    # Give optparse.OptionParser an `add_argument` method for
+    # compatibility with argparse.ArgumentParser
+    try:
+        parser.add_argument = parser.add_option
+    except AttributeError:
+        pass
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='Do not output normal status messages')
+
+    options = parser.parse_args()
+    if isinstance(options, tuple):
+        args = options[0]
+    else:
+        args = options
+    return args
+
 
 if __name__ == "__main__":
     main()
